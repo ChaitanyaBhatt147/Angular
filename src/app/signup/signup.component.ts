bdf97368b352aa92a594425a2d002a40eda95d9f
ChaitanyaBhatt147 <bhattchaitanya43@gmail.com>
2026-02-25 17:38:08 +0530

    done

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+  form: any = {
+    data: {}
+  }
+
+  signup() {
+    console.log('firstName====',this.form.data.firstName);
+    console.log('lastName ==' ,this.form.data.lastName);
+    console.log('login==',this.form.data.login);
+    console.log('password ==',this.form.data.password);
+    console.log('dob ==',this.form.data.dob);
+  }
+}
